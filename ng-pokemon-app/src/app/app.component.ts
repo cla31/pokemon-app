b9d90264afcf2335dd7716d2f214d3bb242a1176
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-03 16:37:45 +0200

    Liaison évènement et propriété

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -11,17 +11,22 @@ import { Pokemon } from './pokemon';
 })
 export class AppComponent implements OnInit {
   pokemonList:Pokemon[] = POKEMONS;  //On indique que Pokemon est un tableau
+  //Pour interagir avec le template sans caster
+  pokemonSelected:Pokemon|undefined;
 
   ngOnInit(){
     console.table(this.pokemonList);  
   }
-  //Avec l'input ds le template, changement du contrat d'interface:
-  selectPokemon(event:MouseEvent){
-    //On est obligé de caster l'évènemement en HTMLInputElement (propre à Angular)
-    // Le "+" transforme tout ce qui est à droite en un NonNullableFormBuilder.
-    const index:number=+(event.target as HTMLInputElement).value;
-    //On demande l'index qu'à passé l'utilisateur:
-    // Je rentre un nombre, je clique sur le nombre, et le Pokémon de la liste s'affiche
-    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  selectPokemon(pokemonId:string){
+   //Je vais chercher le pokémon dont j'ai besoin:
+   const pokemon  : Pokemon|undefined =this.pokemonList.find(pokemon =>pokemon.id == +pokemonId)
+   if(pokemon){
+    console.log(`Vous avez demandé le pokémon ${pokemon.name}`); 
+    this.pokemonSelected = pokemon;
+   }else{
+    console.log(`Vous avez demandé un pokémon qui n'existe pas`); 
+    this.pokemonSelected = pokemon;
+   }
+   
   }
 }
