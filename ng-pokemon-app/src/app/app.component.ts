657b723cbff4f158841299879a0313fb4705a5e2
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-03 13:23:12 +0200

    Récupération des données mockées dans le composant.

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 //import des éléments dont on va avoir besoin dans notre fichier
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 //On utilise l'importation sous la forme d'un décorateur, ça va
 // permettre de construire un composant web avec angular.
@@ -10,20 +12,26 @@ import { Component, OnInit } from '@angular/core';
 //Il affiche la propriété title de l'AppComponent qu'angular va pousser ds le template
 @Component({
   selector: 'app-root',
-  template: `<h1> Welcome to {{pokemonList[1]}}!</h1>`
+  template: `<h1>Liste de Pokémons</h1>`
 })
 export class AppComponent implements OnInit {
-  pokemonList = ['Bulbizarre','Salamèche','Carapuce'];
+  // pokemonList = ['Bulbizarre','Salamèche','Carapuce'];
+  //Récupération de tous les Pokémons dans la constante de Mock:
+  pokemonList:Pokemon[] = POKEMONS;  //On indique que Pokemon est un tableau
 
   ngOnInit(){
     console.table(this.pokemonList);
     //On teste ici l'appel de la méthode
-    this.selectPokemon('Bulbizarre')
+    // this.selectPokemon('Bulbizarre')
+    //Ici je dois désormais écrire un objet métier Pokémon
+    this.selectPokemon(this.pokemonList[0]);
     
   }
   //On peut déclarer des méthodes dans la classe de nos composants
-  selectPokemon(pokemonName:string){
+  //On importe la classe depuis le ficier Pokemon.
+  //Intérêt: structure de données définies en typescript: il s'agit bien d'un pokémon avec point de vies, identifiant etc.
+  selectPokemon(pokemon:Pokemon){
     //syntaxe qui évite la concétanisation et gère les espaces
-    console.log(`Vous avez cliqué sur le pokémon ${pokemonName}`);
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
   }
 }
